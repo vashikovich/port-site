1495160df0bd51a84e3f56108c00a7dd5da7dd0d
Indera-Aji-Waskitho <waskitho@outlook.com>
2024-04-30 16:47:33 +0700

    added contact layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,20 @@ import Cover from "@/components/cover";
 import Services from "@/components/services";
 import Expertise from "@/components/expertise";
 import Projects from "@/components/projects";
+import Contact from "@/components/contact";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`max-w-screen-2xl mx-auto ${inter.className}`}
+      className={`max-w-screen-lg mx-auto ${inter.className}`}
     >
       <Cover />
       <Services />
       <Expertise />
       <Projects />
+      <Contact />
     </main>
   );
 }
